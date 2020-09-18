af1de2ac9820ff7c8b4d4fda857d9b705293419f
Sam Goddard <hello@samgoddard.co.uk>
2020-09-18 13:36:26 +0100

    fridays work

---
+++ components/reviews/README.md
@@ -0,0 +1,5 @@
+## WordPress Integration
+
+When integrating the reviews component into the Adtrak WordPress boilerplate, use the following include to pass WP data into the component:
+
+@todo finish this include properly...
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,10 @@
 let mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
 
+// @TODO - Write some conditional here...
+// This one for dev...
 // mix.postCss('resources/css/main.css', 'public/css', [tailwindcss('./tailwind.config.js')]);
+// This one for prod...
 mix.postCss('resources/css/main.css', 'build/css', [tailwindcss('./tailwind.config.js')]);
 
 mix.browserSync({ proxy: 'localhost:4444', files: ["components/**/*.twig"] });
