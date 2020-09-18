cc35a1d8d848e11f09536e96b346e9204e2e860a
Sam Goddard <hello@samgoddard.co.uk>
2020-09-18 14:33:34 +0100

    tailwind screens map updaed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   },
   theme: {
     screens: {
-			sm: '520px',
-      md: '768px',
-      lg: '1024px',
-			xl: '1280px',
-			'2xl': '1600px'
+      '2xs' : '375px',
+      'xs' : '480px',
+      'sm' : '600px',
+      'md' : '768px',
+      'lg' : '1024px',
+      'xl' : '1280px',
+      '2xl' : '1400px',
+      '3xl' : '1600px',
+      '4xl' : '1900px',
     },
     extend: {
       colors: {
